6f858f513ce1e9beca2832c080bb2be85b08075f
Tishe Eniola <tisheeniola@tishesm2macbook.home>
2024-10-31 20:14:21 -0400

    add: base & remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/amateur-movie-database/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
